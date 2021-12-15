d8945ec9ae7b2960fd5f3a0c94ea45cdc9eef41e
John Munson <johncmunson91@gmail.com>
2021-12-14 18:02:33 -0600

    add preliminary support for mdx and mdx-bundler. this code could use some cleanup and refactoring, but it's an excellent start and accomplishes what I want it to do.

---
+++ content/my-first-post/CustomComponent.tsx
@@ -0,0 +1,14 @@
+const Custom = () => (
+  <div className="marquee">
+    <div className="marquee__inner" aria-hidden="true">
+      <span>Yo</span>
+      <span>dawg,</span>
+      <span>I'm</span>
+      <span>a</span>
+      <span>custom</span>
+      <span>component.</span>
+    </div>
+  </div>
+);
+
+export default Custom;
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,69 @@
+import { useMemo } from "react";
+import { getMDXComponent } from "mdx-bundler/client";
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import { Frontmatter, getAllSlugs, getSinglePost } from "../../utils/mdx";
+import { ParsedUrlQuery } from "querystring";
+
+type Params = ParsedUrlQuery & {
+  slug: string;
+};
+
+type Props = {
+  code: string;
+  frontmatter: Frontmatter;
+};
+
+const Post: NextPage<Props> = ({
+  code,
+  frontmatter,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  // It's generally a good idea to memoize this function call to
+  // avoid re-creating the component every render.
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+
+  return (
+    <>
+      <header className="mb-4">
+        <h1>Title: {frontmatter.title}</h1>
+        <p>Description: {frontmatter.description}</p>
+        <p className="text-xs">
+          (This is the header. It&apos;s part of the layout that is applied to
+          every post.)
+        </p>
+        <hr />
+      </header>
+      <main>
+        <Component />
+      </main>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (
+  context
+) => {
+  const params = context.params!;
+  const post = await getSinglePost(params.slug);
+
+  return {
+    props: { ...post },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = await getAllSlugs();
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+};
+
+export default Post;
