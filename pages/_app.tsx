e041befbaf7d7c8250e06eff9af8b90faa0b8d88
John Munson <johncmunson91@gmail.com>
2021-12-19 23:56:20 -0600

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app"
 // https://nextjs.org/docs/advanced-features/custom-app
 
 const x = 5
+const y = 6
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
