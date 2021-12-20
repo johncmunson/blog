7e47dcd50946316ca3b5653e3391657b58ffa3be
John Munson <johncmunson91@gmail.com>
2021-12-19 23:55:42 -0600

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   "**/*.ts?(x)": "prettier --write --ignore-unknown",
   "**/*.js?(x)": (filenames) => {
-    console.log("FIRED")
+    console.log(filenames)
     return `next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(" --file ")}`
