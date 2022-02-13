a086fbe1523d6c008da6be8de4c89b24c2b22604
John Munson <johncmunson91@gmail.com>
2022-02-13 09:48:18 -0600

    add the content directory to the tailwind config

--- content/2021-12-29-my-first-post/CustomComponent.tsx
+++ content/2021-12-29-my-first-post/CustomComponent.tsx
@@ -1,3 +1,6 @@
+// Notice how custom components in the 'content' directory work
+// with both TailwindCSS and CSS Modules.
+
 const Custom = () => (
   <div className="marquee">
     <div className="marquee__inner" aria-hidden="true">
@@ -5,8 +8,8 @@ const Custom = () => (
       <span>dawg,</span>
       <span>I&apos;m</span>
       <span>a</span>
-      <span>custom</span>
-      <span>component.</span>
+      <span className="text-blue-600">custom</span>
+      <span className="text-blue-600">component.</span>
     </div>
   </div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    // should the "content" directory be added here as well?
+    './content/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
