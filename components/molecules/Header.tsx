851907637d935077b07ccaff64111b448ca3535b
John Munson <johncmunson91@gmail.com>
2022-04-16 22:56:28 -0500

    adjust margin-x for the header

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
   }
 
   return (
-    <div className="mt-9 mx-3">
+    <div className="mt-9 mx-4">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-semibold">Ternary Town</h1>
         <button
