38b333b5a3b7f725e6c664145f96ec6539c24b6c
John Munson <johncmunson91@gmail.com>
2022-04-16 23:25:07 -0500

    make header more responsive and center it with mx-auto

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -10,86 +10,88 @@ export const Header = () => {
   }
 
   return (
-    <div className="mt-9 mx-4">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-semibold">Ternary Town</h1>
-        <button
-          role="button"
-          title="Menu"
-          value="Menu"
-          className={styles.hamburgerMenuButton}
-          onClick={handleMenuClick}
-        >
-          <i
-            className={
-              isMenuOpen
-                ? styles.hamburgerMenuLine1Open
-                : styles.hamburgerMenuLine1Closed
-            }
-          ></i>
-          <i
-            className={
-              isMenuOpen
-                ? styles.hamburgerMenuLine2Open
-                : styles.hamburgerMenuLine2Closed
-            }
-          ></i>
-        </button>
+    <div className="mt-9 mx-4 sm:mt-16 sm:mx-8 md:mt-32 md:mx-20">
+      <div className="max-w-5xl mx-auto">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-semibold">Ternary Town</h1>
+          <button
+            role="button"
+            title="Menu"
+            value="Menu"
+            className={styles.hamburgerMenuButton}
+            onClick={handleMenuClick}
+          >
+            <i
+              className={
+                isMenuOpen
+                  ? styles.hamburgerMenuLine1Open
+                  : styles.hamburgerMenuLine1Closed
+              }
+            ></i>
+            <i
+              className={
+                isMenuOpen
+                  ? styles.hamburgerMenuLine2Open
+                  : styles.hamburgerMenuLine2Closed
+              }
+            ></i>
+          </button>
+        </div>
+        <nav className={isMenuOpen ? styles.drawerOpen : styles.drawerClosed}>
+          <section className={styles.drawerItem}>
+            <h3 className="font-semibold">Most Popular</h3>
+            <ul>
+              <li>On Apps and Coffee</li>
+              <li>End Procrastination</li>
+              <li>Do Presentations Make You Anxious?</li>
+              <li>How To Think Different</li>
+              <li>On Monopolies, Apple, and Epic</li>
+            </ul>
+          </section>
+          <section className={styles.drawerItem}>
+            <h3 className="font-semibold">Trending</h3>
+            <ul>
+              <li>Useful macOS Tricks</li>
+              <li>UX Lessons In Game Design</li>
+              <li>“Ethics” and Ethics</li>
+              <li>Take the Power Back</li>
+              <li>Why we like distractions</li>
+            </ul>
+          </section>
+          <section className={styles.drawerItem}>
+            <h3 className="font-semibold">Subscribe</h3>
+            <ul>
+              <li>Twitter</li>
+              <li>Newsletter</li>
+              <li>RSS</li>
+            </ul>
+          </section>
+          <section className={styles.drawerItem}>
+            <h3 className="font-semibold">Address</h3>
+            <address className="not-italic">
+              3750 Washington Ave
+              <br />
+              St. Louis, MO 63108
+            </address>
+          </section>
+          <section className={styles.drawerItem}>
+            <h3 className="font-semibold">Downloads</h3>
+            <ul>
+              <li>Fonts</li>
+              <li>Templates</li>
+              <li>Apps</li>
+            </ul>
+          </section>
+          <section className={styles.drawerItem}>
+            <h3 className="font-semibold">About</h3>
+            <ul>
+              <li>Mission</li>
+              <li>Core Values</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </section>
+        </nav>
       </div>
-      <nav className={isMenuOpen ? styles.drawerOpen : styles.drawerClosed}>
-        <section className={styles.drawerItem}>
-          <h3 className="font-semibold">Most Popular</h3>
-          <ul>
-            <li>On Apps and Coffee</li>
-            <li>End Procrastination</li>
-            <li>Do Presentations Make You Anxious?</li>
-            <li>How To Think Different</li>
-            <li>On Monopolies, Apple, and Epic</li>
-          </ul>
-        </section>
-        <section className={styles.drawerItem}>
-          <h3 className="font-semibold">Trending</h3>
-          <ul>
-            <li>Useful macOS Tricks</li>
-            <li>UX Lessons In Game Design</li>
-            <li>“Ethics” and Ethics</li>
-            <li>Take the Power Back</li>
-            <li>Why we like distractions</li>
-          </ul>
-        </section>
-        <section className={styles.drawerItem}>
-          <h3 className="font-semibold">Subscribe</h3>
-          <ul>
-            <li>Twitter</li>
-            <li>Newsletter</li>
-            <li>RSS</li>
-          </ul>
-        </section>
-        <section className={styles.drawerItem}>
-          <h3 className="font-semibold">Address</h3>
-          <address className="not-italic">
-            3750 Washington Ave
-            <br />
-            St. Louis, MO 63108
-          </address>
-        </section>
-        <section className={styles.drawerItem}>
-          <h3 className="font-semibold">Downloads</h3>
-          <ul>
-            <li>Fonts</li>
-            <li>Templates</li>
-            <li>Apps</li>
-          </ul>
-        </section>
-        <section className={styles.drawerItem}>
-          <h3 className="font-semibold">About</h3>
-          <ul>
-            <li>Mission</li>
-            <li>Core Values</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </section>
-      </nav>
     </div>
   )
 }
