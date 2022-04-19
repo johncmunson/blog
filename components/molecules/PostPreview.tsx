bee2f4ccc2a7388c5f376c023f7ac3064e28adcf
John Munson <johncmunson91@gmail.com>
2022-04-19 00:48:48 -0500

    add a PostPreview component

---
+++ components/molecules/PostPreview.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+type PostPreviewProps = {
+  date: string
+  title: string
+  previewText: string
+  postSlug: string
+}
+
+export const PostPreview = ({
+  date,
+  title,
+  previewText,
+  postSlug,
+}: PostPreviewProps) => (
+  <>
+    <div className="font-mono text-sm">{date.replaceAll('-', '.')}</div>
+    <div>
+      <Link href={`/posts/${postSlug}`}>
+        <a className="font-medium">{title}</a>
+      </Link>
+    </div>
+    <div>
+      <Link href={`/posts/${postSlug}`}>
+        <a>
+          {previewText} <span className="text-sky-400">&#8594;</span>
+        </a>
+      </Link>
+    </div>
+  </>
+)
