2171fcafbf7894d27b38b7b5ab61c239dc426e17
John Munson <johncmunson91@gmail.com>
2022-10-09 20:28:59 -0500

    add links to the markdown headers. also, refactor the markdown-to-html pipeline.

--- content/2022-04-15-how-to-use-mdx-with-custom-components.md
+++ content/2022-04-15-how-to-use-mdx-with-custom-components.md
@@ -6,6 +6,8 @@ tags: [one, two, three]
 coverPhoto: /stock-photo.png
 ---
 
+[The header of this page](#a-subheading)
+
 ## This is a heading
 
 This is a paragraph... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
--- lib/markdownToHtml.tsx
+++ lib/markdownToHtml.tsx
@@ -16,7 +16,69 @@ import { renderToString } from 'react-dom/server'
 import { BlogPostImage } from '../components/atoms/BlogPostImage'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
 
+// If you ever have the need to write a custom Remark/Rehype plugin, this article is pretty helpful...
+// https://jeffchen.dev/posts/Markdown-Image-Captions/
+
+/**
+ * MARKDOWN TO HTML PIPELINE
+ */
+export default async function markdownToHtml(post: Post) {
+  const reactTree = await unified()
+    // 1. Parse the markdown content into a markdown AST
+    .use(remarkParse)
+    // 2. Markdown parsers often wrap images in a paragraph, e.g. <p><img /></p>
+    //    This is kinda weird and I don't like it. So, unwrap the images.
+    .use(remarkUnwrapImages)
+    // 3. Allow writing plain HTML inside markdown
+    //    https://github.com/remarkjs/remark-rehype#example-supporting-html-in-markdown-properly
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeSanitize)
+    // 4. Add some styles
+    //    Prefer top and left margins, b/c Adam says so -> https://twitter.com/adamwathan/status/1399473286224957442
+    //    Also, we're not including h1 because the title of blog posts is already an h1 and we don't want multiple
+    //    h1 tags on the page. We're not including h4 through h6 because there's really no reason to ever use those
+    //    tags in a blog post. We just need a heading (h2) and a subheading (h3).
+    .use(addClasses, {
+      h2: `${H2}`,
+      h3: `${H3}`,
+      p: `${BODY_TEXT}`,
+      ul: `${UL}`,
+      ol: `${OL}`,
+    })
+    // 5. Add self-referencing links to the headers
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, { behavior: 'wrap' })
+    // 6. Compile HTML AST to a React node. Swap in some React components.
+    .use(rehypeReact, {
+      createElement,
+      Fragment,
+      passNode: true,
+      components: {
+        img: imgToFigureWithCaption,
+      },
+    })
+    // 7. Pass in the raw markdown string to get processed by the above pipeline
+    .process(
+      new VFile({
+        value: post.markdown,
+      })
+    )
+
+  // 8. Compile the React node to an HTML string that can be used with dangerouslySetInnerHTML
+  // Might consider trying out renderToStaticMarkup instead. Could save a few bytes being sent over the wire.
+  // https://reactjs.org/docs/react-dom-server.html#rendertostaticmarkup
+  const htmlString = renderToString(reactTree.result)
+
+  return htmlString
+}
+
+/**
+ * PIPELINE HELPERS
+ */
 type ImgProps = {
   node: Element & { properties: { src: string; alt: string } }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -43,72 +105,23 @@ function extractCaptionFromAltText(altText?: string | null) {
   }
 }
 
-// If you ever have the need to write a custom Remark/Rehype plugin, this article is pretty helpful...
-// https://jeffchen.dev/posts/Markdown-Image-Captions/
-export default async function markdownToHtml(post: Post) {
-  const htmlHast = await unified()
-    .use(remarkParse)
-    .use(remarkUnwrapImages)
-    // Allow writing plain HTML inside markdown
-    // https://github.com/remarkjs/remark-rehype#example-supporting-html-in-markdown-properly
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
-    .use(rehypeSanitize)
-    .use(addClasses, {
-      // Prefer top and left margins, b/c Adam says so -> https://twitter.com/adamwathan/status/1399473286224957442
-      // Also, we're not including h1 because the title of blog posts is already an h1 and we don't want multiple
-      // h1 tags on the page. We're not including h4 through h6 because there's really no reason to ever use those
-      // tags in a blog post. We just need a heading (h2) and a subheading (h3).
-      h2: `${H2}`,
-      h3: `${H3}`,
-      p: `${BODY_TEXT}`,
-      ul: `${UL}`,
-      ol: `${OL}`,
-    })
-    .use(rehypeStringify)
-    .process(
-      new VFile({
-        value: post.markdown,
-      })
-    )
+// Convert an <img /> to a <figure><img /><figcaption /><figure>
+function imgToFigureWithCaption(props: unknown) {
+  const castedProps = props as ImgProps
+  const { node } = castedProps
 
-  const html = htmlHast.toString()
+  const { alt, caption } = extractCaptionFromAltText(node.properties.alt)
 
-  const transformedHtmlHast = await unified()
-    .use(rehypeParse, { fragment: true })
-    .use(rehypeReact, {
-      createElement,
-      Fragment,
-      passNode: true,
-      components: {
-        img: (props: unknown) => {
-          const castedProps = props as ImgProps
-          const { node } = castedProps
-
-          const { alt, caption } = extractCaptionFromAltText(
-            node.properties.alt
-          )
-
-          return (
-            <figure
-              className={`w-11/12 sm:w-5/6 md:w-4/5 lg:w-3/4 mx-auto mt-3 sm:mt-4 md:mt-5 lg:mt-6`}
-            >
-              <BlogPostImage src={node.properties.src} alt={alt} />
-              {caption && (
-                <figcaption className="italic text-center text-xs sm:text-base md:text-lg lg:text-lx">
-                  {caption}
-                </figcaption>
-              )}
-            </figure>
-          )
-        },
-      },
-    })
-    .process(new VFile({ value: html }))
-
-  // Might consider trying out renderToStaticMarkup instead. Could save a few bytes being sent over the wire.
-  // https://reactjs.org/docs/react-dom-server.html#rendertostaticmarkup
-  const transformedHtmlString = renderToString(transformedHtmlHast.result)
-
-  return transformedHtmlString
+  return (
+    <figure
+      className={`w-11/12 sm:w-5/6 md:w-4/5 lg:w-3/4 mx-auto mt-3 sm:mt-4 md:mt-5 lg:mt-6`}
+    >
+      <BlogPostImage src={node.properties.src} alt={alt} />
+      {caption && (
+        <figcaption className="italic text-center text-xs sm:text-base md:text-lg lg:text-lx">
+          {caption}
+        </figcaption>
+      )}
+    </figure>
+  )
 }
