38ad84e8686652042a1a089fe579cb581d0c1d6b
John Munson <johncmunson91@gmail.com>
2022-10-26 18:08:43 -0500

    add a Link to the title of blog posts, so that if you click a sublink on one of the blog post headers you can click the title of the blog post to clear out the url hash. i.e. ternarytown.com/posts/my-post#this-is-a-subheading --> ternarytown.com/posts/my-post#

--- components/atoms/PageHeading.tsx
+++ components/atoms/PageHeading.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
+
 export const PageHeading = ({ children }: { children: React.ReactNode }) => (
   <h1 className="font-semibold tracking-wide text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-    {children}
+    <Link href="#">{children}</Link>
   </h1>
 )
