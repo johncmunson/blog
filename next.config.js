4111a938db05f7717e81a5d124af59dc2351ee7a
John Munson <johncmunson91@gmail.com>
2022-10-29 22:52:50 -0500

    make a few tweaks based on the new defaults in create-next-app@latest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     dirs: ['components', 'context', 'hooks', 'lib', 'pages', 'types'], // https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
   },
 }
+
+module.exports = nextConfig
