8043e455bf8f28b25f26a586a0b366936c61ac66
John Munson <19480078+johncmunson@users.noreply.github.com>
2025-12-03 22:55:19 +0000

    remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
 import Image from "next/image";
 
 const geistSans = localFont({
