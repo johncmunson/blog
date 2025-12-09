1e00f4cee392b12e975f0b5d593a99e913d4e084
John Munson <19480078+johncmunson@users.noreply.github.com>
2025-12-09 20:15:39 +0000

    add a PencilLine icon

---
+++ components/pencil-line-icon.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+
+export function PencilLineIcon({
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("size-6 text-foreground", className)}
+      {...props}
+    >
+      <path d="M13 21h8" />
+      <path d="m15 5 4 4" />
+      <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+    </svg>
+  );
+}
