91a6933cee7c16f4dbbf32d4803ac518c6a10eab
John Munson <19480078+johncmunson@users.noreply.github.com>
2025-12-22 14:29:57 -0600

    testing something out

--- app/twitter-image.tsx
+++
@@ -1,9 +0,0 @@
-import { generateTwitterImage, TWITTER_IMAGE_SIZE } from "@/lib/generate-og-image"
-
-export const alt = "Blog homepage"
-export const size = TWITTER_IMAGE_SIZE
-export const contentType = "image/png"
-
-export default async function Image() {
-  return generateTwitterImage()
-}
