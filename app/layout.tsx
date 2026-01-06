540a5b1eacfbe4ebdc6cc55d1bc059f6fb0fb37a
John Munson <19480078+johncmunson@users.noreply.github.com>
2026-01-06 17:13:39 -0600

    preload the mono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ const geistSans = localFont({
 const geistMono = localFont({
   variable: "--font-geist-mono",
   src: "../public/fonts/GeistMono[wght].ttf",
-  preload: false,
 })
 
 const images = [
