26d20e8285dffbdaf7aa4df80af9772c541f7916
John Munson <19480078+johncmunson@users.noreply.github.com>
2026-01-06 16:53:31 -0600

    fix the issue where inline code and code blocks weren't using the geist-mono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[70ch] m-3 sm:m-4 prose dark:prose-invert prose-hr:my-10`}
       >
-        <main className="font-sans">{children}</main>
+        <main className="font-sans prose-code:font-mono prose-pre:font-mono">{children}</main>
         <hr />
         <footer className="font-sans text-xs flex items-center">
           {/* aria-label provides the accessible name for this link. Images use alt="" 
